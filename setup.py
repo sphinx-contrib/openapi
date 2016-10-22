dc0a95658b34f00c657fb41ced558665100865ac
Ihor Kalnytskyi <igor@kalnitsky.org>
2016-10-22 12:17:15 +0300

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(os.path.join(here, 'README.rst'), 'r', encoding='utf-8') as f:
 
 setup(
     name='sphinxcontrib-openapi',
-    version='0.1.0',
+    version='0.2.0',
     description='OpenAPI (fka Swagger) spec renderer for Sphinx',
     long_description=long_description,
     license='BSD',
