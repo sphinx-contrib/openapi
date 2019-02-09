532b43c3bd867d2a3f9ba8dea230293c0017ad09
Olivier Heurtier <olivier.heurtier@free.fr>
2019-02-09 16:36:26 +0100

    Remove duplicate code - add options to command line utility

--- sphinxcontrib/openapi/cmdline.py
+++ sphinxcontrib/openapi/cmdline.py
@@ -2,24 +2,9 @@
 from __future__ import unicode_literals
 
 import argparse
-import collections
-import io
 import logging
 
-import yaml
-
-from sphinxcontrib.openapi import openapi20
-from sphinxcontrib.openapi import openapi30
-
-
-class _YamlOrderedLoader(yaml.SafeLoader):
-    pass
-
-
-_YamlOrderedLoader.add_constructor(
-    yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-    lambda loader, node: collections.OrderedDict(loader.construct_pairs(node))
-)
+from sphinxcontrib.openapi import directive
 
 
 def main():
@@ -33,6 +18,27 @@ def main():
         default=logging.INFO,
         dest='level',
         help="Logging level")
+    parser.add_argument(
+        "-e", "--encoding",
+        action='store',
+        default="ascii",
+        dest='encoding',
+        help="Source file encoding")
+    parser.add_argument(
+        "-p", "--paths",
+        action='append',
+        dest='paths',
+        help="Endpoints to be rendered")
+    parser.add_argument(
+        "-x", "--examples",
+        action='store_true',
+        dest='examples',
+        help="Include examples")
+    parser.add_argument(
+        "-g", "--group",
+        action='store_true',
+        dest='group',
+        help="Group paths by tag")
     parser.add_argument(
         "-i", "--input",
         dest='input',
@@ -48,27 +54,20 @@ def main():
     options = parser.parse_args()
     logging.getLogger().setLevel(options.level)
 
-    with io.open(options.input, 'rt', encoding="ascii") as stream:
-        spec = yaml.load(stream, _YamlOrderedLoader)
+    openapi_options = {}
+    if options.paths:
+        openapi_options['paths'] = options.paths
+    if options.examples:
+        openapi_options['examples'] = True
+    if options.group:
+        openapi_options['group'] = True
+    openapihttpdomain, spec = \
+        directive.get_openapihttpdomain(
+            openapi_options,
+            options.input,
+            options.encoding)
 
-    # URI parameter is crucial for resolving relative references. So
-    # we need to set this option properly as it's used later down the
-    # stack.
-    openapi_options = {'uri': 'file://%s' % options.input}
-
-    # We support both OpenAPI 2.0 (f.k.a. Swagger) and OpenAPI 3.0.0, so
-    # determine which version we are parsing here.
-    spec_version = spec.get('openapi', spec.get('swagger', '2.0'))
-    if spec_version.startswith('2.'):
-        logging.info("OpenAPI 2.x Specification")
-        openapihttpdomain = openapi20.openapihttpdomain
-    elif spec_version.startswith('3.'):
-        logging.info("OpenAPI 3.x Specification")
-        openapihttpdomain = openapi30.openapihttpdomain
-    else:
-        raise ValueError('Unsupported OpenAPI version (%s)' % spec_version)
-
-    for line in openapihttpdomain(spec, examples=True, **openapi_options):
+    for line in openapihttpdomain(spec, **openapi_options):
         options.output.write(line+'\n')
         logging.debug(line)
 
