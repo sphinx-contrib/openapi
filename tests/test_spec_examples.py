a00b827272382e4c5753ec0e5381e62333f7609a
Stephen Finucane <stephen@that.guru>
2022-12-07 10:11:17 +0000

    Add tests for OpenAPI 3.1 schemas
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- tests/test_spec_examples.py
+++ tests/test_spec_examples.py
@@ -43,11 +43,30 @@ def test_openapi2_success(tmpdir, run_sphinx, spec):
         'v3.0',
         '*.yaml')
 ))
-def test_openapi3_success(tmpdir, run_sphinx, spec, render_examples,
-                          render_request, group_paths):
+def test_openapi30_success(tmpdir, run_sphinx, spec, render_examples,
+                           render_request, group_paths):
     py.path.local(spec).copy(tmpdir.join('src', 'test-spec.yml'))
     run_sphinx('test-spec.yml', options={
         'examples': render_examples,
         'request': render_request,
         'group': group_paths,
+    })
+
+
+@pytest.mark.parametrize('render_examples', [False, True])
+@pytest.mark.parametrize('render_request', [False, True])
+@pytest.mark.parametrize('spec', glob.glob(
+    os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        'OpenAPI-Specification',
+        'examples',
+        'v3.1',
+        '*.yaml')
+))
+def test_openapi31_success(tmpdir, run_sphinx, spec, render_examples,
+                           render_request):
+    py.path.local(spec).copy(tmpdir.join('src', 'test-spec.yml'))
+    run_sphinx('test-spec.yml', options={
+        'examples': render_examples,
+        'request': render_request,
     })
